c41fc5d4c89e8f770ff7886a87b383f530805e4d
Raissa Assis <raissaassis017@gmail.com>
2023-09-14 15:40:58 -0300

    create button with props

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,18 @@
 import './Header.css'
 import { NavLink } from "react-router-dom"
+import Button from '../ui/Button'
 
 const Header = () => {
   return (
     <header>
-          <img id="logo" src="../public/VolleyChampPNG.png" alt="VolleyChamp" />
+          <img id="logo" src="../VolleyChampPNG.png" alt="VolleyChamp" />
         <nav>
             <ul>
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="/quem-somos">Quem Somos</NavLink></li>
                 <li><NavLink to="/contato">Contato</NavLink></li>
+                <li><Button variant="primary">Login</Button></li>
+                <li><Button variant="secondary">Cadastre-se</Button></li>
             </ul>
         </nav>
     </header>
