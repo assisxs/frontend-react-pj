a4d32c00a395277762506093d40274f9c3df0e2c
Raissa Assis <raissaassis017@gmail.com>
2023-09-18 17:59:22 -0300

    create card

---
+++ src/components/ui/CardUser.jsx
@@ -0,0 +1,15 @@
+import './CardUser.css'
+
+const CardUser = () => {
+  return (
+    <div className="card-user">
+        <img src="https://avatars.githubusercontent.com/u/126916518?v=4" alt="Raissa Assis" />
+        <div>
+            <h3>Raissa Assis</h3>
+            <span>raissaassis@gmai.com</span>
+        </div>
+    </div>
+  )
+}
+
+export default CardUser
