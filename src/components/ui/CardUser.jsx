82148fde5ec2d77d1d22c3fa4e3ecf5d737030f5
Raissa Assis <raissaassis017@gmail.com>
2023-10-09 15:47:22 -0300

    add delete user

--- src/components/ui/CardUser.jsx
+++ src/components/ui/CardUser.jsx
@@ -1,15 +1,40 @@
 import './CardUser.css'
+import { IoTrashOutline as IconTrash } from 'react-icons/io5'
+import { FiEdit as IconEdit } from 'react-icons/fi'
 
-const CardUser = ({ user }) => {
-  return (
-    <div className="card-user">
-      <img src={user.photo} alt={user.name} />
-      <div>
-        <h3>{user.name}</h3>
-        <span>{user.email}</span>
-      </div>
+const CardUser = ({ user, users, setUsers }) => {
+
+  const handleDelete = async (id) => {
+    const response = await fetch('http://localhost:3000/user', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id: id })
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      if (data?.sucess) {
+        alert("Usuário apagado com sucesso!")
+        const newUsers = users.filter((user) => user.id != id)
+        setUsers(newUsers)
+      }
+    }
+  }
+
+
+return (
+  <div className="card-user">
+    <img src={user.photo} alt={user.name} />
+    <div className='infos'>
+      <h3>{user.name}</h3>
+      <span>{user.email}</span>
+      <IconEdit className='icon-edit' />
+      <IconTrash className='icon-trash' onClick={() => handleDelete(user.id)}/>
     </div>
-  )
+  </div>
+)
 }
 
 export default CardUser
