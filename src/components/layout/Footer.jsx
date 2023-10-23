2a51e4709357fa1da1d8bc4c5cfd121bf07adef2
Raissa Assis <raissaassis017@gmail.com>
2023-10-23 16:24:09 -0300

    sample zustand state manager

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,14 @@
 import "./Footer.css"
+import useUserStore from '../../store/userStore.js'
 
 const Footer = () => {
+
+  const nameUser = useUserStore((state) => state.name)
+
   return (
     <footer>
         <span>VolleyChamp @ 2023</span>
+        <span>VolleyChamp @ 2023 - Logado como: {nameUser}</span>
     </footer>
   )
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,20 +1,37 @@
 import './Header.css'
 import { NavLink } from "react-router-dom"
 import Button from '../ui/Button'
+import useUserStore from '../../store/userStore.js'
 
 const Header = () => {
+
+  const nameUser = useUserStore((state) => state.name)
+  const isLogged = useUserStore((state) => state.isLogged)
+  const loginUser = useUserStore((state) => state.login)
+  const logoutUser = useUserStore((state) => state.logout)
+
   return (
     <header>
-          <img id="logo" src="../VolleyChampPNG.png" alt="VolleyChamp" />
-        <nav>
-            <ul>
-                <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="/quem-somos">Quem Somos</NavLink></li>
-                <li><NavLink to="/contato">Contato</NavLink></li>
-                <li><Button variant="primary">Login</Button></li>
-                <li><Button variant="secondary">Cadastre-se</Button></li>
-            </ul>
-        </nav>
+      <img id="logo" src="../VolleyChampPNG.png" alt="VolleyChamp" />
+      <h1>{nameUser}</h1>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/quem-somos">Quem Somos</NavLink></li>
+          <li><NavLink to="/contato">Contato</NavLink></li>
+          {
+            isLogged ?
+              <li><Button variant="primary" onClick={() => logoutUser({})}>Logout</Button></li>
+              :
+              <li><Button variant="primary" onClick={() => loginUser({
+                name: "Raissa",
+                email: "raissa@gmail.com",
+                photo: "..."
+              })}>Login</Button></li>
+          }
+          <li><Button variant="secondary">Cadastre-se</Button></li>
+        </ul>
+      </nav>
     </header>
   )
 }
