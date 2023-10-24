2718feca2a9870fb5c66ba8d5da6de9b9c1ffe1d
Raissa <raissaassis017@gmail.com>
2023-10-24 07:45:11 -0300

    add prop-types

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,15 @@
 import './Button.css'
+import PropTypes from 'prop-types';
 
 const Button = ({children, variant, ...rest}) => {
     return (
         <button {...rest} className={variant}>{children}</button>
     )
 }
+
+Button.propTypes = {
+    children: PropTypes.node, // Valide a prop children como um node
+    variant: PropTypes.string, // Valide a prop variant como uma string (ou o tipo apropriado)
+};
 
 export default Button
